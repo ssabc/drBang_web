ee2d6ffd265528f627d7025462de13cdbf4ee54e
ssabc <abczhaoshan@163.com>
2017-11-23 17:29:47 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import catList from '@/components/catList'
+import catList from '@/views/catIndex'
+import sxDetail from '@/views/sxDetail'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'catList',
       component: catList
+    },
+    {
+      path: '/sxDetail/:kx/:sx/:sxName/:kxName',
+      name: 'sxDetail',
+      component: sxDetail
     }
   ]
 })
